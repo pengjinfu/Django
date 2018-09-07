d4e063c85d36731ef0bf1f51bb4b1cefd9ade5dc
pengjinfu <18317916706@163.com>
2018-09-07 14:41:15 +0800

    在子应用里写了子路由

--- django_study/users/views.py
+++ django_study/users/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    """视图函数"""
+    """
+    index视图
+    :param request: 包含了请求信息的请求对象
+    :return: 响应对象
+    """
     return HttpResponse("Hello Django")
